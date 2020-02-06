c9e7c7fb9bee1b3a729b7f3239063da2a551390f
shen-sijie <597479384@qq.com>
2020-02-06 21:54:32 +0000

    third commit

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,11 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
     'rango',
-=======
-   
->>>>>>> b0700c3bd26b29848ee8146570484025f09c0973
 ]
 
 MIDDLEWARE = [
@@ -71,7 +68,7 @@ TEMPLATES = [
         },
     },
 ]
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
