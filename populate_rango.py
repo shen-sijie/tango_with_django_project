72894482c89c6bb6649742860cc057638e4adb44
shen-sijie <597479384@qq.com>
2020-02-12 12:11:50 +0000

    fifth commit3

--- populate_rango.py
+++ populate_rango.py
@@ -14,7 +14,7 @@ def populate():
          'url':'http://www.greenteapress.com/thinkpython/'},
         {'title':'Learn Python in 10 Minutes',
          'url':'http://www.korokithakis.net/tutorials/python/'} ]
-    
+
     django_pages = [
         {'title':'Official Django Tutorial',
          'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
@@ -22,22 +22,22 @@ def populate():
          'url':'http://www.djangorocks.com/'},
         {'title':'How to Tango with Django',
          'url':'http://www.tangowithdjango.com/'} ]
-    
+
     other_pages = [
         {'title':'Bottle',
          'url':'http://bottlepy.org/docs/dev/'},
         {'title':'Flask',
          'url':'http://flask.pocoo.org'} ]
-    
+
     cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
             'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
             'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
-    
+
     for cat, cat_data in cats.items():
         c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
             add_page(c, p['title'], p['url'])
-    
+
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
             print(f'- {c}: {p}')
--- rango/views.py
+++ rango/views.py
@@ -2,10 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    # Refer to the TwD book for more information on how this updated view works.
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    # Spoiler: you don't need to pass a context dictionary here.
     return render(request, 'rango/about.html')
