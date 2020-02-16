fa34b3aab4d01d2ef1fbe859abcefa0b07c5b1ae
shen-sijie <597479384@qq.com>
2020-02-16 18:22:17 +0000

    chapter 9

--- rango/views.py
+++ rango/views.py
@@ -42,6 +42,7 @@ def show_category(request, category_name_slug):
 
     return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
 
@@ -56,6 +57,7 @@ def add_category(request):
 
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -139,15 +141,9 @@ def user_login(request):
     else:
         return render(request, 'rango/login.html')
 
-def some_view(request):
-   if not request.user.is_authenticated():
-      return HttpResponse("You are logged in.")
-   else:
-      return HttpResponse("You are not logged in.")
-
 @login_required
 def restricted(request):
-   return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'rango/restricted.html')
 
 @login_required
 def user_logout(request):
